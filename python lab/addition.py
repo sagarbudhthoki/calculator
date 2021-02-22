ae3f223585042cf931145aaf264a47511c766234
Sagar Budhathoki <sagarbudhathoki128@gmail.com>
2021-02-22 22:03:17 +0545

    area

--- python lab/addition.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("Enter the first number:"))
-num2 = int(input("Enter the second number:"))
-num3 = int(input("Enter the third number:"))
-num = num1+num2+num3;
-print(num)
---
+++ python lab/area.py
@@ -0,0 +1,4 @@
+length = float(input("Enter the length:"))
+breadth = float(input("Enter the breadth:"))
+area = (length*breadth)//2
+print(area)
