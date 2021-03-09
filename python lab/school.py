cebd2f22d148e0dcf2a5d46311578b6173117bd5
Sagar Budhathoki <sagarbudhathoki128@gmail.com>
2021-03-09 11:12:53 +0545

    calculation of the BMI index

---
+++ python lab/school.py
@@ -0,0 +1,8 @@
+mass = float(input("enter the mass of the person in kg:"))
+height = float(input("enter the height of the perrson in the meter:"))
+BMI = (mass/(height*height))
+print(f"The BMI index of a person is (BMI)")
+
+
+
+
