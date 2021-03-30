8448ca67f33150fc0aa87059df2e10d2d31b8b57
Sagar Budhathoki <sagarbudhathoki128@gmail.com>
2021-03-30 12:03:36 +0545

    name

---
+++ python lab/name.py
@@ -0,0 +1,7 @@
+name = input("the name of any subject:")
+if len(name)<3:
+    print("the name is of 3 characters:")
+elif len(name)>50:
+    print(" the name is of maximum characters:")
+else:
+    print("it looks good:")
